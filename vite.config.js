0873d93a2c5bfcd9a79c4e45b7b5447a832164d6
sidkulkarni <siddheshdilipkulkarni@gmail.com>
2026-01-05 00:29:48 +0530

    Fix routing

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/",   // REQUIRED for custom domains
   build: {
-    outDir: "docs",
-    emptyOutDir: true,
+    outDir: "docs", // REQUIRED for GitHub Pages
   },
 });
-

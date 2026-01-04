dc401c548ad891eac8b3692a8d0e7aa2baaf2b07
sidkulkarni <siddheshdilipkulkarni@gmail.com>
2026-01-05 00:01:53 +0530

    Add auto deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // IMPORTANT for custom domain
-})
+  base: "/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
+});
+

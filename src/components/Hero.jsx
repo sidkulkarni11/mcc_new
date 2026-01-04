732532151b9e43e0489062bc0ed6186469371e8a
sidkulkarni <siddheshdilipkulkarni@gmail.com>
2026-01-05 00:38:17 +0530

    Fix GitHub Pages routing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,6 +14,7 @@ import img4 from "../webphotos/nisha4.jpeg";
 import img6 from "../webphotos/nisha6.jpeg";
 
 export default function Hero() {
+// eslint-disable-next-line no-unused-vars
     const images = [img1, img2, img3, img4, img6];
 
     const whatsappMessage = encodeURIComponent(

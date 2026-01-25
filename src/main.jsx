10db0a3c1412bf1f0904004c3a638411b080286e
sidkulkarni <siddheshdilipkulkarni@gmail.com>
2026-01-25 18:33:58 +0530

    Fix direct route opening on GitHub Pages (BrowserRouter no hash)

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import { HashRouter } from "react-router-dom";
-
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+
+function RedirectHandler({ children }) {
+    React.useEffect(() => {
+        const redirect = sessionStorage.redirect;
+        delete sessionStorage.redirect;
+
+        if (redirect && redirect !== window.location.pathname) {
+            window.history.replaceState(null, "", redirect);
+        }
+    }, []);
+
+    return children;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <BrowserRouter>
+            <RedirectHandler>
+                <App />
+            </RedirectHandler>
+        </BrowserRouter>
     </React.StrictMode>
 );

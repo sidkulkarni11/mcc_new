c1f5b9bbac0307dae817313d8effeeed7b5d9546
sidkulkarni <siddheshdilipkulkarni@gmail.com>
2026-02-07 11:06:43 +0530

    CR

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import RedirectHandler from "./components/RedirectHandler.jsx";
+// import RedirectHandler from "./components/RedirectHandler.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
-            <RedirectHandler />
+            {/* Remove RedirectHandler permanently */}
             <App />
         </BrowserRouter>
     </React.StrictMode>
